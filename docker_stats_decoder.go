753878b06fee748cf0a40610446d843ab62e1296
tommyvicananza <javier.molina@bq.com>
2016-02-01 19:15:52 +0100

    trying to fix

--- docker_stats_decoder.go
+++ docker_stats_decoder.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/mozilla-services/heka/pipeline"
@@ -14,10 +15,12 @@ type DockerStatsDecoder struct {
 }
 
 func (input *DockerStatsDecoder) Init(config interface{}) error {
+	fmt.Printf("Decoder Init")
 	return nil
 }
 
 func (input *DockerStatsDecoder) Decode(pack *pipeline.PipelinePack) (packs []*pipeline.PipelinePack, err error) {
+	fmt.Printf("Decode")
 	var buf bytes.Buffer
 	pack.Message.SetPayload(string(buf.Bytes()))
 
@@ -26,6 +29,8 @@ func (input *DockerStatsDecoder) Decode(pack *pipeline.PipelinePack) (packs []*p
 }
 
 func (*DockerStatsDecoder) decode(pack *pipeline.PipelinePack) bytes.Buffer {
+
+	fmt.Printf("decode")
 	var stats = make(map[string]string)
 
 	reader := bufio.NewReader(strings.NewReader(pack.Message.GetPayload()))
@@ -40,6 +45,7 @@ func (*DockerStatsDecoder) decode(pack *pipeline.PipelinePack) bytes.Buffer {
 		stats[fields[0]] = fields[1]
 	}
 
+	fmt.Println("hola")
 	json, _ := json.Marshal(stats)
 	return *bytes.NewBuffer(json)
 }
