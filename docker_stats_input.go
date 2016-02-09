62913b3467290b3d427c2759506f7f343e26dbba
tommyvicananza <javier.molina@bq.com>
2016-02-09 18:53:41 +0100

    trying to fix

--- docker_stats_input.go
+++ docker_stats_input.go
@@ -128,7 +128,7 @@ func (input *DockerStatsInput) Run(runner pipeline.InputRunner,
 			pack.Message.SetUuid(uuid.NewRandom())
 			pack.Message.SetTimestamp(time.Now().UnixNano())
 			pack.Message.SetType("docker.stats")
-			pack.Message.SetHostname(hostname)
+			pack.Message.SetHostname(strings.Replace(hostname, "-", "_", -1))
 			pack.Message.SetPayload(fmt.Sprintf("hostname %s\ncontainer_id %s\ncpu %.2f\nmem_usage %d\nmem_limit %d\nmem %.2f\nnet_input %d\nnet_output %d\nblock_input %d\nblock_output %d",
 				hostname,
 				input.cacheHostnames[container.ID],
